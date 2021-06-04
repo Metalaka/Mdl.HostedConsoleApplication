944aeb5802cb890069fdea21bc3e09a17fc7636f
Metalaka <matthieu.de.laubrie@gmail.com>
2021-06-04 17:36:09 +0200

    Add project

---
+++ Mdl.HostedConsoleApplication/HostBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Mdl.HostedConsoleApplication
+{
+    public static class HostBuilderExtensions
+    {
+        public static async Task RunConsoleApplicationAsync<T>(this IHostBuilder builder) where T : class, IHostedConsoleApplication
+        {
+            builder.ConfigureServices((_, services) => services.AddSingleton<IHostedConsoleApplication, T>());
+            
+            using var serviceScope = builder.Build().Services.CreateScope();
+            
+            var application = serviceScope.ServiceProvider.GetRequiredService<IHostedConsoleApplication>();
+            await application.RunAsync();
+        }
+    }
+}
---
+++ Mdl.HostedConsoleApplication/IHostedApplication.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mdl.HostedConsoleApplication
+{
+    public interface IHostedConsoleApplication
+    {
+        async Task RunAsync() => await RunAsync(CancellationToken.None);
+        Task RunAsync(CancellationToken cancellationToken);
+    }
+}
